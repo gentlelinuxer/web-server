c5c78b53add8bde18a48f0530b3062531126c047
Taeung Song <treeze.taeung@gmail.com>
2021-02-10 00:08:59 +0900

    web-server version 2

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const http = require('http');
 const hostname = '0.0.0.0';
 const port = 80;
+var os = require("os");
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.write('Hello World !\n');
-    res.end();
+    res.write(os.hostname());
+    res.end('\n');
 });
 
 server.listen(port, hostname, () => {
